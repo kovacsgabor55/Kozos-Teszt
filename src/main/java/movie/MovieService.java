a70f3db24e4917f3517aad5bf4a448ca6eca44bb
ShadowDragon <kovacsgabor55@gmail.com>
2021-06-09 16:46:47 +0200

    search newest film

--- src/main/java/movie/MovieService.java
+++ src/main/java/movie/MovieService.java
@@ -1,12 +1,29 @@
 package movie;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MovieService {
     private List<Movie> movies = new ArrayList<>();
 
     public void save(Movie movie) {
         movies.add(movie);
+    }
+
+    public List<Movie> getMovies() {
+        return new ArrayList<>(movies);
+    }
+
+    public Movie newestMovie() {
+        if (movies.isEmpty()) {
+            throw new IllegalArgumentException("Movie list most be empty!");
+        }
+        List<Movie> sorted = movies.stream()
+                .sorted(Comparator.comparing(Movie::getReleaseDate))
+                .collect(Collectors.toList());
+        return sorted.get(0);
     }
 }
